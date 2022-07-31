8e56ae4f73efe4a6a380f8108e9fc44970597690
rizkyzhang <rizkyzhangdev@gmail.com>
2022-07-31 11:28:58 +0700

    feat: init rpc server

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+)
+
+type Product struct {
+	Id int
+	Name string
+	Price float64
+	Stock uint
+	IsActive bool
+}
+
+type API struct {}
+
+var products []Product
+
+func (a *API) GetProducts(empty string, reply *[]Product) error {
+	*reply = products
+
+	return nil
+}
+
+func main() {
+	products = []Product{
+		{
+			Id: 0,
+			Name: "Phone",
+			Price: 150.0,
+			Stock: 23,
+			IsActive: true,
+		},
+		{
+			Id: 1,
+			Name: "Laptop",
+			Price: 675.28,
+			Stock: 7,
+			IsActive: true,
+		},
+		{
+			Id: 2,
+			Name: "VGA",
+			Price: 1000.13,
+			Stock: 10,
+			IsActive: false,
+		},
+	}
+
+	fmt.Println(products)
+	
+	api := new(API)
+	err := rpc.Register(api)
+	if (err != nil) {
+		log.Fatal("Error register RPC:", err)
+	}
+	
+	rpc.HandleHTTP()
+	
+	listener, err := net.Listen("tcp", ":4040")
+	if (err != nil) {
+		log.Fatal("Error listener:", err)
+	}
+	
+	log.Printf("Serving RPC on port %d", 4040)
+
+	err = http.Serve(listener, nil)
+	if (err != nil) {
+		log.Fatal("Error serving:", err)
+	}
+}
